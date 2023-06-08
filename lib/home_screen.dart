83fc5263eef829916271cde560b754bc2223b0f9
Paras Shenmare <shenmareparas@gmail.com>
2023-06-09 01:52:21 +0530

    Product Screen Changes

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -20,93 +20,113 @@ class HomeScreen extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ElevatedButton(
-                  onPressed: null,
-                  style: ButtonStyle(
-                    side: MaterialStateProperty.all<BorderSide>(
-                      const BorderSide(
-                        color: Colors.grey,
-                        width: 0.5,
-                      ),
-                    ),
-                  ),
-                  child: const Text(
-                    'Fresh Releases',
-                    style: TextStyle(
-                        color: Colors.grey, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  'You navigate simply\nby using your eyes,\nhands, and voice.',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  'So you can do the things you love in ways never before possible.',
-                  style: TextStyle(color: Colors.grey, fontSize: 12),
-                ),
-                const SizedBox(height: 420),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const FloatingActionButton(
-                      heroTag: "BlackBox",
-                      backgroundColor: Color(0xFF2F2F2F),
-                      shape: CircleBorder(),
-                      elevation: 0,
-                      tooltip: "Logo",
+                    ElevatedButton(
                       onPressed: null,
-                      child: Icon(
-                        Icons.apple,
-                        size: 35,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    SizedBox(
-                      width: 270,
-                      child: FloatingActionButton(
-                        heroTag: "RedBox",
-                        elevation: 0,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const Product(),
-                            ),
-                          );
-                        },
-                        backgroundColor: Colors.red,
-                        child: const Text(
-                          'Buy Now',
-                          style: TextStyle(color: Colors.white, fontSize: 18),
+                      style: ButtonStyle(
+                        side: MaterialStateProperty.all<BorderSide>(
+                          const BorderSide(
+                            color: Colors.grey,
+                            width: 0.5,
+                          ),
                         ),
                       ),
+                      child: const Text(
+                        'Fresh Releases',
+                        style: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'You navigate simply\nby using your eyes,\nhands, and voice.',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'So you can do the things you love in ways never before possible.',
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
                     ),
                   ],
                 ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Column(
                   children: [
-                    Text(
-                      'Already have an account?',
-                      style: TextStyle(color: Colors.grey, fontSize: 15),
+                    Row(
+                      children: [
+                        const FloatingActionButton(
+                          heroTag: "BlackBox",
+                          backgroundColor: Color(0xFF2F2F2F),
+                          shape: CircleBorder(),
+                          elevation: 0,
+                          tooltip: "Logo",
+                          onPressed: null,
+                          child: Icon(
+                            Icons.apple,
+                            size: 35,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: FloatingActionButton(
+                            heroTag: "RedBox",
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25)),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Product(),
+                                ),
+                              );
+                            },
+                            backgroundColor: const Color(0xFFFF2B13),
+                            child: const Padding(
+                              padding: EdgeInsets.only(left: 20, right: 20),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Buy Now',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 18),
+                                  ),
+                                  Icon(Icons.north_east, color: Colors.white)
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    TextButton(
-                        onPressed: null,
-                        child: Text(
-                          "Sign In",
-                          style: TextStyle(
-                              color: Colors.red,
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold),
-                        )),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account?',
+                          style: TextStyle(color: Colors.grey, fontSize: 15),
+                        ),
+                        TextButton(
+                            onPressed: null,
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  color: Color(0xFFFF2B13),
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                      ],
+                    ),
                   ],
                 )
               ],
--- lib/product.dart
+++ lib/product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Product extends StatelessWidget {
   const Product({super.key});
@@ -10,85 +11,116 @@ class Product extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage(
-                "assets/product.jpg",
-              ),
-              fit: BoxFit.cover),
+            image: AssetImage(
+              "assets/product.jpg",
+            ),
+            fit: BoxFit.contain,
+          ),
         ),
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Row(
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Apple Vision Pro',
-                      style: TextStyle(
-                          color: Color(0xFF252525),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30),
-                    ),
-                    SizedBox(width: 10),
-                    FloatingActionButton(
-                      shape: CircleBorder(),
-                      elevation: 0,
-                      heroTag: "NewBox",
-                      backgroundColor: Colors.red,
-                      onPressed: null,
-                      child: Text(
-                        'New',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
-                ),
-                const Text(
-                  'Available early next year in the U.S.',
-                  style: TextStyle(color: Colors.grey, fontSize: 12),
-                ),
-                const SizedBox(height: 550),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 270,
-                      height: 50,
-                      child: FloatingActionButton(
-                        heroTag: "BlackBox",
-                        elevation: 0,
-                        backgroundColor: const Color(0xFF2F2F2F),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25)),
-                        onPressed: () {},
-                        child: const Text(
-                          "Preorder",
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Already have an account?',
-                      style: TextStyle(color: Colors.grey, fontSize: 15),
-                    ),
-                    TextButton(
-                        onPressed: null,
-                        child: Text(
-                          "Sign In",
+                    Row(
+                      children: [
+                        Text(
+                          'Apple Vision Pro',
                           style: TextStyle(
-                              color: Colors.red,
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold),
-                        )),
+                              color: Color(0xFF252525),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        ),
+                        SizedBox(width: 10),
+                        FloatingActionButton(
+                          shape: CircleBorder(),
+                          elevation: 0,
+                          heroTag: "NewBox",
+                          backgroundColor: Color(0xFFFF2B13),
+                          onPressed: null,
+                          child: Text(
+                            'New',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Available early next year in the U.S.',
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "\$3499",
+                          style: GoogleFonts.inter(
+                            fontSize: 40,
+                            color: const Color(0xFF2F2F2F),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(width: 15),
+                        const Expanded(
+                          child: Text(
+                            "The stadard VAT rate may be charged folloing the law of your country",
+                            style: TextStyle(color: Colors.grey, fontSize: 12),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: FloatingActionButton(
+                            heroTag: "BlackBox",
+                            elevation: 0,
+                            backgroundColor: const Color(0xFF2F2F2F),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25)),
+                            onPressed: () {},
+                            child: const Text(
+                              "Preorder",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 15, bottom: 15),
+                      child: Divider(color: Colors.grey),
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account?',
+                          style: TextStyle(color: Colors.grey, fontSize: 15),
+                        ),
+                        TextButton(
+                            onPressed: null,
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  color: Color(0xFFFF2B13),
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                      ],
+                    ),
                   ],
                 )
               ],
